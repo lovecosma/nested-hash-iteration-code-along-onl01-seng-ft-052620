2ffd6f94c51089e80e377993a294532c22ea1776
Matthew Williams <williamsmatthew333@gmail.com>
2020-06-04 20:20:23 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -17,7 +17,7 @@ require 'pry'
 def remove_strawberry(contacts)
   contacts.each do |person, data|
     if person == "Freddy Mercury"
-      person.each do |attribute, value|
+      "Freddy Mercury".each do |attribute, value|
         if attribute == "favorite_ice_cream_flavors:"
           puts "hey"
         end
